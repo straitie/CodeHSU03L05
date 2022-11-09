c51b110a4accff0ecdf05fa0514ac06c13fc4c2d
straitie <straitie@wlhs.wlwv.k12.or.us>
2022-11-09 09:29:57 -0800

    Improved month checks

--- src/Salmon.java
+++ src/Salmon.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 /*
 Your task is to write a program that takes the month of the year as an integer and outputs if it is
@@ -21,21 +22,14 @@ public class Salmon {
         // Ask user for month of year as an integer
         System.out.println("Enter the month of the year as a number: ");
         int month = input.nextInt();
-        // Use if/else if/else statement to determine if it is spawning season
-        if(month<3){
-            System.out.println("Not spawning season");
+        if(month >= 3 && month <= 6 ){
+            System.out.println("Spring spawning season.");
         }
-        else if(month < 7){
-            System.out.println("Spring spawning season");
+        else if (month >= 9 && month <= 11){
+            System.out.println("Fall spawning season.");
         }
-        else if(month < 9){
-            System.out.println("Not spawning season");
-        }
-        else if(month < 12){
-            System.out.println("Fall spawning season");
-        }
-        else{
-            System.out.println("Not spawning season");
+        else {
+            System.out.println("Not spawning season.");
         }
 
     }
